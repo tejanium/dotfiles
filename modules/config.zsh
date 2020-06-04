6f88a719949f5740e3730470062ac4dec05ce305
Teja Sophista <tejanium@yahoo.com>
2020-06-04 16:25:58 +0100

    use antibody

--- modules/config.zsh
+++ modules/config.zsh
@@ -9,3 +9,24 @@ test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell
 
 export PATH="/usr/local/opt/mysql@5.6/bin:$PATH"
 # export PATH="/usr/local/opt/node@10/bin:$PATH"
+
+# Plugins
+zstyle ':prezto:*:*' color 'yes'
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+
+export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=10
+
+# Completion
+autoload -Uz compinit
+if [[ -n ${ZDOTDIR}/.zcompdump(#qN.mh+24) ]]; then
+	compinit;
+else
+	compinit -C;
+fi;
+
+# Utils
+export EDITOR='vim'
