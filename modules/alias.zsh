07eb3fec8cff1ef7f09aadad4e86db7b62dc07b3
Teja Sophista <tejanium@yahoo.com>
2023-06-13 13:42:19 +0100

    better gprune

--- modules/alias.zsh
+++ modules/alias.zsh
@@ -1,9 +1,12 @@
 alias gst='git status'
 alias gco='git checkout'
 alias cat=ccat
-alias gprune='gco main && git pull && comm -12 <(git branch | sed "s/ *//g") <(git remote prune origin | sed "s/^.*origin\///g") | xargs -L1 -J % git branch -D %'
+alias gprune="gco trunk && git pull --prune && git branch --format '%(refname:short) %(upstream:track)' | awk '\$2 == \"[gone]\" { print \$1 }' | xargs -r git branch -D"
 alias ss='spring stop'
 alias bi='bundle install'
 alias be='bundle exec'
 alias brewup='brew update && brew upgrade && brew upgrade --cask && brew autoremove && brew cleanup'
 alias unl='ss && docker-compose down && docker-compose up -d'
+alias code=code-insiders
+
+alias dpsql='docker exec -it --user postgres global-pg psql'
